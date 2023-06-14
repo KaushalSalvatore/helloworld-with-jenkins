d808ee14acf84c52450cf35417418e26e48d1e60
kaushal <kaushalpandey455@gmail.com>
2023-06-14 18:19:38 +0530

    print listr value

---
+++ print_list.py
@@ -0,0 +1,17 @@
+import os 
+List1 = [8,9,5,6,7,8,1,10]
+List1.reverse()
+print("reversed list",List1)
+
+List2 = [21,34,56,78,9,23,45,75,768,65]
+List2.sort()
+print("sorted list",List2)
+
+
+List3 = []
+List3 = List1.copy()
+print("new list",List3)
+
+
+indexvalue = List2[2:4]
+print("index are", indexvalue)
